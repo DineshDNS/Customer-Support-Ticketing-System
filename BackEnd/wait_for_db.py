6e61dad8acaebe7fd500dfb080fc5d70f7b9e79d
Dinesh T <dineshkarthikklm92@gmail.com>
2026-04-29 07:39:01 +0530

    added ci/cd advanced

--- BackEnd/wait_for_db.py
+++ BackEnd/wait_for_db.py
@@ -4,14 +4,15 @@ import psycopg2
 while True:
     try:
         conn = psycopg2.connect(
-            dbname="ticketing_db",   
-            user="postgres",         
-            password="667254",       
-            host="db",
+            dbname="ticketing_db",
+            user="postgres",
+            password="667254",
+            host="postgres",
             port="5432"
         )
+        conn.close()
         print("✅ Database is ready!")
         break
-    except psycopg2.OperationalError:
-        print("⏳ Waiting for database...")
+    except psycopg2.OperationalError as e:
+        print("⏳ Waiting for database...", e)
         time.sleep(2)
